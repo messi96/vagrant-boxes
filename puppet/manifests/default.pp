7fc78ef71f7ae8e4e5c3b63d8b2752a99f73fe26
Prateep Bandharangshi <prateep@waratek.com>
2014-04-22 10:47:59 +0100

    Move class declarations to common module

---
+++ puppet/manifests/default.pp
@@ -0,0 +1,27 @@
+
+node default {
+    include common
+}
+
+node 'kali' {
+    include common
+
+    host { 'target':
+        ensure => 'present',
+        ip     => '192.168.100.101'
+
+    }
+}
+
+node 'target' {
+    class { "waratek": }
+
+    class { 'waratek::cloudvm-tgz':
+        version     =>  '2.6.1.alpha.4-001'
+    }
+
+    class { 'tomcat':
+        tomcat_version => '7.0.53',
+        struts_version => '2.2.1.1'
+    }
+}
