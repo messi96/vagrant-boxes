a1ef91c6671aa1fc803380b8e801a776632a9a0d
Prateep Bandharangshi <prateep@waratek.com>
2014-07-11 16:21:42 +0100

    user cloudvm

--- puppet/modules/tomcat/templates/bin/startup.sh
+++ puppet/modules/tomcat/templates/bin/startup.sh
@@ -2,7 +2,7 @@
 
 export CATALINA_HOME="/opt/apache-tomcat"
 export CATALINA_BASE="<%= @base_dir %>"
-export JAVA_HOME="/usr/lib/jvm/java-openjdk/jre"
+export JAVA_HOME="/usr/lib/jvm/java-waratek/jre"
 
 ${CATALINA_HOME}/bin/startup.sh
 
