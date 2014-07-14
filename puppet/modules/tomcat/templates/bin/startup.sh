1e55dca41bbc4f55e092f36f8bc009b41a9e9d7c
Prateep Bandharangshi <prateep@waratek.com>
2014-07-14 14:36:11 +0100

    Don't cleanup prior to restart

--- puppet/modules/tomcat/templates/bin/startup.sh
+++ puppet/modules/tomcat/templates/bin/startup.sh
@@ -4,7 +4,5 @@ export CATALINA_HOME="/opt/apache-tomcat"
 export CATALINA_BASE="<%= @base_dir %>"
 export JAVA_HOME="/usr/lib/jvm/java-waratek/jre"
 
-${JAVA_HOME}/bin/jvckill --jvc=<%= @title %> > /dev/null 2>&1
-${JAVA_HOME}/bin/jvcundefine --jvc=<%= @title %> > /dev/null 2>&1
 ${CATALINA_HOME}/bin/startup.sh
 
