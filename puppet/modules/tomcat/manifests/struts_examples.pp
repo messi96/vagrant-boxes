ff2cd56c2ce9ae040ddda2ee479c76dd847ba86c
Prateep Bandharangshi <prateep@waratek.com>
2014-07-14 15:31:33 +0100

    refactor struts module

--- puppet/modules/tomcat/manifests/struts_examples.pp
+++ puppet/modules/tomcat/manifests/struts_examples.pp
@@ -1,69 +1,68 @@
-class tomcat::struts_examples ( $tomcat_version, $struts_version ) {
+class tomcat::struts_examples ( $webapps_dir, $struts_version, $owner, $group ) {
 
-    $tomcat_dir = "apache-tomcat-${tomcat_version}"
+    $tmp_dir = "/tmp"
 
     $struts_examples_dir = "struts-${struts_version}"
-    $struts_examples_archive = "${struts_examples_dir}-apps.zip"
+    $struts_examples_zip = "struts-${struts_version}-apps.zip"
     $struts_examples_url = "https://archive.apache.org/dist/struts/examples/struts-${struts_version}-apps.zip"
 
-    exec { 'download-struts-examples':
-        command     =>  "/usr/bin/curl $struts_examples_url -o /home/${vagrantuser}/${struts_examples_archive}",
-        cwd         =>  "/home/${vagrantuser}/",
-        creates     =>  "/home/${vagrantuser}/${struts_examples_archive}",
-        onlyif      =>  "/usr/bin/test ! -r /home/${vagrantuser}/${tomcat_dir}/webapps/struts2-blank.war",
-        user        =>  "${vagrantuser}",
-        group       =>  "${vagrantuser}"
+    exec { "download-struts-examples":
+        command     =>  "/usr/bin/curl $struts_examples_url -o ${tmp_dir}/${struts_examples_zip}",
+        cwd         =>  "${tmp_dir}",
+        creates     =>  "${tmp_dir}/${struts_examples_zip}",
+        onlyif      =>  "/usr/bin/test ! -r ${webapps_dir}/struts2-blank.war",
+        user        =>  "${owner}",
+        group       =>  "${group}"
     }
 
-    exec { 'extract-struts-examples':
-        command     =>  "/usr/bin/unzip /home/${vagrantuser}/${struts_examples_archive}",
-        cwd         =>  "/home/${vagrantuser}/",
-        creates     =>  "/home/${vagrantuser}/$struts_examples_dir/NOTICE.txt",
-        onlyif      =>  "/usr/bin/test ! -r /home/${vagrantuser}/${tomcat_dir}/webapps/struts2-blank.war",
-        user        =>  "${vagrantuser}",
-        group       =>  "${vagrantuser}",
-        require     =>  Exec[ 'download-struts-examples', 'extract-tomcat' ]
+    exec { "extract-struts-examples":
+        command     =>  "/usr/bin/unzip ${tmp_dir}/${struts_examples_zip}",
+        cwd         =>  "${tmp_dir}",
+        creates     =>  "${tmp_dir}/${struts_examples_dir}/NOTICE.txt",
+        onlyif      =>  "/usr/bin/test ! -r ${webapps_dir}/struts2-blank.war",
+        user        =>  "${owner}",
+        group       =>  "${group}",
+        require     =>  Exec[ "download-struts-examples" ]
     }
 
-    exec { 'deploy-struts-examples':
-        command     =>  "/bin/cp /home/${vagrantuser}/${struts_examples_dir}/apps/struts2-blank.war /home/${vagrantuser}/${tomcat_dir}/webapps",
-        creates     =>  "/home/${vagrantuser}/${tomcat_dir}/webapps/struts2-blank.war",
-        user        =>  "${vagrantuser}",
-        group       =>  "${vagrantuser}",
-        require     =>  Exec[ 'extract-tomcat', 'extract-struts-examples' ]
+    exec { "deploy-struts-examples":
+        command     =>  "/bin/cp ${tmp_dir}/${struts_examples_dir}/apps/struts2-blank.war ${webapps_dir}",
+        creates     =>  "${webapps_dir}/struts2-blank.war",
+        user        =>  "${owner}",
+        group       =>  "${group}",
+        require     =>  Exec[ "extract-struts-examples" ]
     }
 
-    file { "/home/${vagrantuser}/${tomcat_dir}/webapps/struts2-blank":
+    file { "${webapps_dir}/struts2-blank":
         ensure      =>  "directory",
-        owner       =>  "${vagrantuser}",
-        group       =>  "${vagrantuser}",
-        mode        =>  0755,
-        require     =>  Exec[ "extract-tomcat"]
+        owner       =>  "${owner}",
+        group       =>  "${group}",
+        mode        =>  0755
     }
 
-    exec { 'extract-struts-warfile':
-        command     =>  "/usr/bin/jar -xf /home/${vagrantuser}/${tomcat_dir}/webapps/struts2-blank.war",
-        cwd         =>  "/home/${vagrantuser}/${tomcat_dir}/webapps/struts2-blank",
-        creates     =>  "/home/${vagrantuser}/${tomcat_dir}/webapps/struts2-blank/index.html",
-        user        =>  "${vagrantuser}",
-        group       =>  "${vagrantuser}",
-        require     =>  [ Exec[ 'deploy-struts-examples' ], File[ "/home/${vagrantuser}/${tomcat_dir}/webapps/struts2-blank" ] ]
+    exec { "extract-struts-warfile":
+        command     =>  "/usr/bin/jar -xf ${webapps_dir}/struts2-blank.war",
+        cwd         =>  "${webapps_dir}/struts2-blank",
+        creates     =>  "${webapps_dir}/struts2-blank/index.html",
+        user        =>  "${owner}",
+        group       =>  "${group}",
+        require     =>  [ Exec[ "deploy-struts-examples" ], File[ "${webapps_dir}/struts2-blank" ] ]
     }
 
-    exec { 'cleanup-struts-examples':
-        command     =>  "/bin/rm -rf ${struts_examples_dir} ${struts_examples_archive}",
-        cwd         =>  "/home/${vagrantuser}/",
-        onlyif      =>  "/usr/bin/test -r /home/${vagrantuser}/${struts_examples_dir} || /usr/bin/test -r /home/${vagrantuser}/${struts_examples_archive}",
-        require     =>  Exec[ 'deploy-struts-examples' ]
+    exec { "cleanup-struts-examples":
+        command     =>  "/bin/rm -rf ${struts_examples_dir} ${struts_examples_zip}",
+        cwd         =>  "${tmp_dir}",
+        onlyif      =>  "/usr/bin/test -r ${tmp_dir}/${struts_examples_dir} || /usr/bin/test -r ${tmp_dir}/${struts_examples_zip}",
+        require     =>  Exec[ "deploy-struts-examples" ]
     }
 
-    file { "/home/${vagrantuser}/${tomcat_dir}/webapps/struts2-blank/example/HelloWorld.jsp":
+    file { "${webapps_dir}/struts2-blank/example/HelloWorld.jsp":
         ensure      =>  "present",
-        owner       =>  "${vagrantuser}",
-        group       =>  "${vagrantuser}",
+        owner       =>  "${owner}",
+        group       =>  "${group}",
         mode        =>  0644,
         source      =>  "puppet:///modules/tomcat/HelloWorld.jsp",
-        require     =>  Exec[ 'extract-struts-warfile' ]
+        require     =>  Exec[ "extract-struts-warfile" ]
     }
 
 }
