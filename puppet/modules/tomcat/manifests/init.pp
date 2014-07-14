411c0d0e978b1e13bf4ef27a49ca250dac8da68d
Prateep Bandharangshi <prateep@waratek.com>
2014-07-14 15:21:30 +0100

    Change single quotes to double quotes

--- puppet/modules/tomcat/manifests/init.pp
+++ puppet/modules/tomcat/manifests/init.pp
@@ -5,18 +5,18 @@ class tomcat ( $tomcat_version ) {
     $tomcat_tgz = "apache-tomcat-${tomcat_version}.tar.gz"
     $tomcat_download_url = "http://archive.apache.org/dist/tomcat/tomcat-7/v${tomcat_version}/bin/${tomcat_tgz}"
 
-    exec { 'download-tomcat':
+    exec { "download-tomcat":
         command     =>  "/usr/bin/curl $tomcat_download_url -o ${base_dir}/${tomcat_tgz}",
         cwd         =>  "$base_dir",
         creates     =>  "$base_dir/${tomcat_tgz}",
         onlyif      =>  "/usr/bin/test ! -r ${install_dir}/RUNNING.txt",
     }
 
-    exec { 'extract-tomcat':
+    exec { "extract-tomcat":
         command     =>  "/bin/tar zxf ${base_dir}/${tomcat_tgz}",
         cwd         =>  "$base_dir",
         creates     =>  "$install_dir/RUNNING.txt",
-        require     =>  Exec[ 'download-tomcat' ]
+        require     =>  Exec[ "download-tomcat" ]
     }
 
     file { "/opt/apache-tomcat":
@@ -25,10 +25,10 @@ class tomcat ( $tomcat_version ) {
     }
 
 
-    exec { 'cleanup-tomcat':
+    exec { "cleanup-tomcat":
         command     =>  "/bin/rm -f ${base_dir}/${tomcat_tgz}",
         onlyif      =>  "/usr/bin/test -r ${base_dir}/${tomcat_tgz}",
-        require     =>  Exec[ 'extract-tomcat' ]
+        require     =>  Exec[ "extract-tomcat" ]
     }
 
 }
