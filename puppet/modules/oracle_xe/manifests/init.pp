04a5f5802b16a2b4c9d83bcf4f66933eff155e2e
Prateep Bandharangshi <prateep@waratek.com>
2014-07-15 22:05:31 +0100

    Create swapfile on EC2

--- puppet/modules/oracle_xe/manifests/init.pp
+++ puppet/modules/oracle_xe/manifests/init.pp
@@ -1,5 +1,22 @@
 class oracle_xe ( $version ) {
 
+    $swapfile = "/var/swapfile.xe"
+
+    # Create swapfile on EC2
+    if ($ec2_ami_id) {
+        exec { "create-swapfile":
+            command => "/bin/dd if=/dev/zero of=${swapfile} bs=1M count=2560",
+            creates => "${swapfile}",
+            notify  => Exec["enable-swap"]
+        }
+
+        exec { "enable-swap":
+            command     => "/sbin/mkswap ${swapfile} && /sbin/swapon ${swapfile}",
+            unless      => "/sbin/swapon -s | /bin/grep ${swapfile}",
+            before      => Exec["install-oracle-xe"],
+            require     => Exec["create-swapfile"]
+        }
+    }
 
     exec { "install-oracle-xe":
         command  => "/bin/rpm -i /vagrant/synced_folder/oracle-xe-${version}.x86_64.rpm || /bin/rpm -i http://rpm/vagrant/oracle-xe-${version}.x86_64.rpm",
