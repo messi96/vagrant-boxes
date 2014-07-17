447df74b603f08a3485cc8cbf1372142e5372ff8
Prateep Bandharangshi <prateep@waratek.com>
2014-07-17 10:45:04 +0100

    Install tomcat to sub directory

--- puppet/modules/waratek/manifests/demo_basic.pp
+++ puppet/modules/waratek/manifests/demo_basic.pp
@@ -14,29 +14,36 @@ class waratek::demo_basic() {
         tomcat_version => "7.0.54"
     }
 
+    file { "/home/${vagrantuser}/tomcat":
+        ensure => "directory",
+        owner  => "${vagrantuser}",
+        group  => "${vagrantuser}",
+        mode   => "0755"
+    }
+
     tomcat::tomcat_instance { "tomcat1":
-        base_dir => "/home/${vagrantuser}/tomcat1",
+        base_dir => "/home/${vagrantuser}/tomcat/tomcat1",
         port     => "8081",
         owner    => "${vagrantuser}",
         group    => "${vagrantuser}"
     }
 
     tomcat::tomcat_instance { "tomcat2":
-        base_dir => "/home/${vagrantuser}/tomcat2",
+        base_dir => "/home/${vagrantuser}/tomcat/tomcat2",
         port     => "8082",
         owner    => "${vagrantuser}",
         group    => "${vagrantuser}"
     }
 
     tomcat::tomcat_instance { "tomcat3":
-        base_dir => "/home/${vagrantuser}/tomcat3",
+        base_dir => "/home/${vagrantuser}/tomcat/tomcat3",
         port     => "8083",
         owner    => "${vagrantuser}",
         group    => "${vagrantuser}"
     }
 
     tomcat::tomcat_instance { "tomcat4":
-        base_dir => "/home/${vagrantuser}/tomcat4",
+        base_dir => "/home/${vagrantuser}/tomcat/tomcat4",
         port     => "8084",
         owner    => "${vagrantuser}",
         group    => "${vagrantuser}"
