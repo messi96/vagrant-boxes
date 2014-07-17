fe8e0a5ed4ea8d5d3988ebdc39db6d3835ee8bcc
Prateep Bandharangshi <prateep@waratek.com>
2014-07-17 16:22:32 +0100

    Refactor to use /synced_folder

--- puppet/modules/waratek/manifests/license.pp
+++ puppet/modules/waratek/manifests/license.pp
@@ -17,8 +17,8 @@ class waratek::license() {
     }
 
     exec { 'install-license':
-        command => "/bin/cp /vagrant/LICENSE_KEY /var/lib/javad",
-        onlyif  => "/usr/bin/test -r /vagrant/LICENSE_KEY",
+        command => "/bin/cp /synced_folder/LICENSE_KEY /var/lib/javad",
+        onlyif  => "/usr/bin/test -r /synced_folder/LICENSE_KEY",
         creates => "/var/lib/javad/LICENSE_KEY",
         require => File[ '/var/lib/javad' ]
     }
