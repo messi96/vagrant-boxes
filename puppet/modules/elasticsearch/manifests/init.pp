4ba44a41fcb88706049eee96f9ed5949c1a86ef3
Prateep Bandharangshi <prateep@waratek.com>
2014-08-14 11:54:24 +0100

    Install elasticsearch

--- puppet/modules/elasticsearch/manifests/init.pp
+++ puppet/modules/elasticsearch/manifests/init.pp
@@ -1,15 +1,26 @@
 class elasticsearch ( $version ) {
 
+    $elasticsearch_rpm_url = "https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-${version}.noarch.rpm"
+
     package { "elasticsearch":
         ensure   => "installed",
         provider => "rpm",
-        source   => "https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-${version}.noarch.rpm"
+        source   => "$elasticsearch_rpm_url"
     }
 
     service { "elasticsearch":
 	    ensure  => "running",
 	    enable  => "true",
-	    require => Package["elasticsearch"]
+	    require => Package["elasticsearch"],
+        notify  => Exec["elasticsearch-dummy-data"]
+    }
+
+    # Ensure there's some dummy data in the index:
+    exec { "elasticsearch-dummy-data":
+        command     => "/usr/bin/curl -XPUT 'localhost:9200/test/test/1' -d '{ \"message\": \"Hello, World!\" }'",
+        refreshonly => "true",
+        tries       => 10,
+        try_sleep   => 10
     }
 
 }
