c4619581dbd32b8676e6c42fa3cc102aa5c9f4a5
Prateep Bandharangshi <prateep@waratek.com>
2014-08-25 12:35:31 +0100

    Refactor sqlmap module

---
+++ puppet/modules/sqlmap/manifests/init.pp
@@ -0,0 +1,15 @@
+class sqlmap {
+
+  vcsrepo { '/opt/sqlmap':
+    ensure   => present,
+    provider => git,
+    source   => 'https://github.com/sqlmapproject/sqlmap.git',
+  }
+
+  vcsrepo { '/var/www/html/sqlmap':
+    ensure   => present,
+    provider => git,
+    source   => 'https://github.com/sqlmapproject/testenv.git',
+  }
+
+}
