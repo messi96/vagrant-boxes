94e1c51734456d7da6df87d5ad32e1c87778ce53
Prateep Bandharangshi <prateep@waratek.com>
2014-08-25 12:34:49 +0100

    Move demo config into profiles module

---
+++ puppet/modules/profiles/manifests/demo_security/heisenberg.pp
@@ -0,0 +1,34 @@
+class profiles::demo_security::heisenberg {
+
+    include '::oracle_xe'
+    include '::sqlmap'
+
+    file { "/root/sqlmap":
+        ensure => "directory"
+    }
+
+    file { "/root/sqlmap/data.sql":
+        source  => "puppet:///modules/sqlmap/data.sql"
+    }
+
+    file { "/root/sqlmap/create_user.sql":
+        source  => "puppet:///modules/sqlmap/create_user.sql"
+    }
+
+    file { "/root/sqlmap/system.sql":
+        source  => "puppet:///modules/sqlmap/system.sql"
+    }
+
+    file { "/root/sqlmap/setupdb.sh":
+        source  => "puppet:///modules/sqlmap/setupdb.sh",
+        mode    => "0755"
+    }
+
+    exec { "setup-db":
+        command     => "/root/sqlmap/setupdb.sh",
+        refreshonly => "true",
+        require     => [ File["/root/sqlmap/data.sql", "/root/sqlmap/create_user.sql", "/root/sqlmap/system.sql", "/root/sqlmap/setupdb.sh"] ],
+        notify      => Service["oracle-xe"]
+    }
+
+}
