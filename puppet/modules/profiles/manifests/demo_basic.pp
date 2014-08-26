e10b02f2b13568344eb3caaa30537080788425a7
Prateep Bandharangshi <prateep@waratek.com>
2014-08-26 10:39:44 +0100

    Remove commented block

--- puppet/modules/profiles/manifests/demo_basic.pp
+++ puppet/modules/profiles/manifests/demo_basic.pp
@@ -1,10 +1,5 @@
 class profiles::demo_basic inherits profiles::base {
 
-  # class { "waratek": }
-
-  # class { "waratek::cloudvm-rpm":
-  #   version => "2.6.5.GA.2-103"
-  # }
 
   class { "waratek::elasticat":
     version   =>  "1.3.2-86"
