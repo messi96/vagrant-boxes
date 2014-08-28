3b7c9e4bea5303499701a8589c7de93c0dd353fb
Prateep Bandharangshi <prateep@waratek.com>
2014-08-28 11:07:48 +0100

    Move tomcat config to hiera

--- puppet/modules/profiles/manifests/demo_security/tomcat.pp
+++ puppet/modules/profiles/manifests/demo_security/tomcat.pp
@@ -1,26 +1,34 @@
 class profiles::demo_security::tomcat {
 
-  $catalina_base = '/home/vagrant/tomcat'
-  $version       = '7.0.55'
-  $source_url    = "https://archive.apache.org/dist/tomcat/tomcat-7/v7.0.55/bin/apache-tomcat-${version}.tar.gz"
+  $tomcat_instances = hiera('tomcat::instances', {})
+  create_resources('tomcat::instance', $tomcat_instances)
 
-  class { '::tomcat':
-    user          => "$vagrantuser",
-    group         => "$vagrantuser",
-    manage_user   => false,
-    manage_group  => false
-  } ->
+  $tomcat_wars = hiera('tomcat::wars', {})
+  create_resources('tomcat::war', $tomcat_wars)
+
+  $catalina_base = $tomcat_instances[tomcat1][catalina_base]
+  notice ($catalina_base)
+
+  # $version       = '7.0.55'
+  # $source_url    = "https://archive.apache.org/dist/tomcat/tomcat-7/v7.0.55/bin/apache-tomcat-${version}.tar.gz"
+
+  # class { '::tomcat':
+  #   user          => "$vagrantuser",
+  #   group         => "$vagrantuser",
+  #   manage_user   => false,
+  #   manage_group  => false
+  # } ->
   
-  tomcat::instance { 'tomcat-demo':
-    catalina_base => "$catalina_base",
-    source_url    => "$source_url"
-  } ->
+  # tomcat::instance { 'tomcat-demo':
+  #   catalina_base => "$catalina_base",
+  #   source_url    => "$source_url"
+  # } ->
 
-  # Waratek test application
-  tomcat::war { 'HeisenbergTestApp.war':,
-    catalina_base => "$catalina_base",
-    war_source    => '/synced_folder/HeisenbergTestApp.war'
-  } -> 
+  # # Waratek test application
+  # tomcat::war { 'HeisenbergTestApp.war':,
+  #   catalina_base => "$catalina_base",
+  #   war_source    => '/synced_folder/HeisenbergTestApp.war'
+  # } -> 
 
   # Struts blank application
   staging::deploy { 'struts-2.2.1.1-apps.zip':
