b65053e215d377f0e812e0e1926769a7c1e2afc2
Prateep Bandharangshi <prateep@waratek.com>
2014-08-29 11:52:42 +0100

    Move package install to base module

--- puppet/modules/profiles/manifests/base.pp
+++ puppet/modules/profiles/manifests/base.pp
@@ -2,27 +2,4 @@ class profiles::base {
 
     include '::ssh'
 
-    class { 'java':
-      package => 'java-1.6.0-openjdk-devel'
-    }
-
-    package { [
-        "bc",
-        "byobu",
-        "htop",
-        "unzip",
-        "vim-enhanced",
-        "wget"
-        ]:
-            ensure => "installed"
-    }
-
-    package { "git":
-        ensure      =>  "installed",
-        name        =>  $operatingsystem ? {
-            "SLES"  =>  "git-core",
-            default =>  "git"
-        }
-    }
-
 }
