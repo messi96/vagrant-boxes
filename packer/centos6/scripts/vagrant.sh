36873981e99afcbe805999e95ba2885931e6adbb
Prateep Bandharangshi <prateep@waratek.com>
2014-08-30 17:54:43 +0100

    Setup vagrant via puppet

--- packer/centos6/scripts/vagrant.sh
+++ packer/centos6/scripts/vagrant.sh
@@ -1,25 +1,6 @@
 #!/bin/bash
 
-# Set up Vagrant.
-date > /etc/vagrant_box_build_time
-
-# Create the user vagrant with password vagrant
-useradd -G wheel -m -s /bin/bash vagrant
-echo "vagrant" | passwd --stdin vagrant
-
-# Install vagrant key
-echo "[*] Installing vagrant ssh key"
-mkdir -pm 700 /home/vagrant/.ssh
-curl -Lo /home/vagrant/.ssh/authorized_keys 'https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub'
-chmod 0600 /home/vagrant/.ssh/authorized_keys
-chown -R vagrant:vagrant /home/vagrant/.ssh
 
 # Set up sudo
 sed -i.orig '/Defaults\s*requiretty/d' /etc/sudoers
-
-( cat <<'EOP'
-%vagrant ALL=NOPASSWD:ALL
-EOP
-) > /etc/sudoers.d/vagrant
-chmod 0440 /etc/sudoers.d/vagrant
 
