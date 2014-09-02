04e4cf6b02afacffd23ae83c75d32d316202c096
Prateep Bandharangshi <prateep@waratek.com>
2014-09-02 15:48:21 +0100

    Create new demo_security module

---
+++ puppet/modules/demo_security/manifests/heisenberg.pp
@@ -0,0 +1,60 @@
+# == Class: demo_security
+#
+# Full description of class demo_security here.
+#
+# === Parameters
+#
+# Document parameters here.
+#
+# [*sample_parameter*]
+#   Explanation of what this parameter affects and what it defaults to.
+#   e.g. "Specify one or more upstream ntp servers as an array."
+#
+# === Variables
+#
+# Here you should define a list of variables that this module would require.
+#
+# [*sample_variable*]
+#   Explanation of how this variable affects the funtion of this class and if
+#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
+#   External Node Classifier as a comma separated list of hostnames." (Note,
+#   global variables should be avoided in favor of class parameters as
+#   of Puppet 2.6.)
+#
+# === Examples
+#
+#  class { demo_security:
+#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
+#  }
+#
+# === Authors
+#
+# Author Name <author@domain.com>
+#
+# === Copyright
+#
+# Copyright 2014 Your name here, unless otherwise noted.
+#
+class demo_security::heisenberg {
+
+  include '::oracle_xe'
+
+  file { '/root/heisenberg':
+    ensure => 'directory'
+  } ->
+
+  file { '/root/heisenberg/setupdb.sql':
+    source  => 'puppet:///modules/profiles/heisenberg/setupdb.sql'
+  } ~>
+
+  exec { 'setup-db':
+    command     => '/u01/app/oracle/product/11.2.0/xe/bin/sqlplus SYS/testpass@//127.0.0.1:1521/XE AS SYSDBA < /root/heisenberg/setupdb.sql',
+    environment => [ 'ORACLE_HOME=/u01/app/oracle/product/11.2.0/xe',
+                     'ORACLE_SID=XE',
+                     'NLS_LANG=ENGLISH_IRELAND.AL32UTF8',
+                     'PATH=/u01/app/oracle/product/11.2.0/xe/bin:/usr/local/sbin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin' ],
+    refreshonly => true,
+    require     => Service['oracle-xe']
+  }
+
+}
---
+++ puppet/modules/demo_security/manifests/init.pp
@@ -0,0 +1,71 @@
+# == Class: demo_security
+#
+# Full description of class demo_security here.
+#
+# === Parameters
+#
+# Document parameters here.
+#
+# [*sample_parameter*]
+#   Explanation of what this parameter affects and what it defaults to.
+#   e.g. "Specify one or more upstream ntp servers as an array."
+#
+# === Variables
+#
+# Here you should define a list of variables that this module would require.
+#
+# [*sample_variable*]
+#   Explanation of how this variable affects the funtion of this class and if
+#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
+#   External Node Classifier as a comma separated list of hostnames." (Note,
+#   global variables should be avoided in favor of class parameters as
+#   of Puppet 2.6.)
+#
+# === Examples
+#
+#  class { demo_security:
+#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
+#  }
+#
+# === Authors
+#
+# Author Name <author@domain.com>
+#
+# === Copyright
+#
+# Copyright 2014 Your name here, unless otherwise noted.
+#
+class demo_security {
+
+  file { "/home/${vagrantuser}/demo":
+    ensure => "directory",
+    owner  => "${vagrantuser}",
+    group  => "${vagrantuser}",
+    mode   => 0755
+  }
+
+  file { "/home/${vagrantuser}/demo/reload_rules.sh":
+    ensure => "file",
+    owner  => "${vagrantuser}",
+    group  => "${vagrantuser}",
+    mode   => 0755,
+    source => "puppet:///modules/demo_security/reload_rules.sh",
+  }
+
+  file { "/home/${vagrantuser}/demo/restart.sh":
+    ensure => "file",
+    owner  => "${vagrantuser}",
+    group  => "${vagrantuser}",
+    mode   => 0755,
+    source => "puppet:///modules/demo_security/restart.sh",
+  }
+
+  file { "/home/${vagrantuser}/demo/rules.jaf":
+    ensure => "file",
+    owner  => "${vagrantuser}",
+    group  => "${vagrantuser}",
+    mode   => 0644,
+    source => "puppet:///modules/demo_security/rules.jaf",
+  }
+
+}
