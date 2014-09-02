eb9f4bc9789cfa9e312a68a13e9042ca05440813
Prateep Bandharangshi <prateep@waratek.com>
2014-09-02 14:58:16 +0100

    Remove comments

--- puppet/modules/profiles/manifests/demo_security.pp
+++ puppet/modules/profiles/manifests/demo_security.pp
@@ -1,11 +1,5 @@
 class profiles::demo_security inherits profiles::base {
 
-  # class { "waratek": }
-
-  # class { "waratek::cloudvm-rpm":
-  #   version => "2.6.5.GA.2.JAS-104"
-  # }
-
   file { "/home/${vagrantuser}/demo":
     ensure => "directory",
     owner  => "${vagrantuser}",
