21f8428c1a5c8d7b34e6268ffd391bc7c26c065c
Prateep Bandharangshi <prateep@waratek.com>
2014-09-16 12:48:10 +0100

    apt-update before package installs

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -2,5 +2,13 @@
 File { backup => false }
 Package { allow_virtual => false }
 
+if ($osfamily == 'Debian') {
+  exec { 'apt-update':
+    command => '/usr/bin/apt-get update'
+  }
+
+  Exec['apt-update'] -> Package <| |>
+}
+
 hiera_include('classes')
 
