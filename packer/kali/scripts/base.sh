4fff6ea4696c576877b2034c3a67ad5b11695f0a
Prateep Bandharangshi <prateep@waratek.com>
2014-09-18 15:16:57 +0100

    Enable postgresql and metasploit via puppet

--- packer/kali/scripts/base.sh
+++ packer/kali/scripts/base.sh
@@ -10,11 +10,6 @@ apt-get -y update
 echo "[*] Upgrading"
 apt-get -y dist-upgrade
 
-# Enable metasploit and postgresql services
-echo "[*] Enabling metasploit and postgresql services"
-update-rc.d postgresql enable
-update-rc.d metasploit enable
-
 # Enable gdm autologin
 echo "[*] Enabling gdm autlogin"
 sed -i.orig -r -e 's/^#\s+(AutomaticLogin.*)/\1/' /etc/gdm3/daemon.conf
