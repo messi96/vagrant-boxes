39b03260bbc3fffb05227c58130e1686baff7224
Prateep Bandharangshi <prateep@waratek.com>
2014-10-09 14:39:14 +0100

    use new splunk dashboard module

--- puppet/modules/demo_security/manifests/centos.pp
+++ puppet/modules/demo_security/manifests/centos.pp
@@ -83,14 +83,4 @@ class demo_security::centos {
     enable => false
   }
 
-  if defined (Package['splunk']) {
-    vcsrepo { '/opt/splunk/etc/apps/waratek':
-      ensure   => present,
-      provider => git,
-      source   => 'https://github.com/prateepb/splunk-test-app.git',
-      require  => Package['splunk'],
-      notify   => Service['splunk']
-    }
-  }
-  
 }
