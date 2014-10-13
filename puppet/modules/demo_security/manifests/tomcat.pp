5b5aabcad5cb3ca15c834a87a69eb40ff48c2dd3
Prateep Bandharangshi <prateep@waratek.com>
2014-10-13 15:02:12 +0100

    Fix version in source_url

--- puppet/modules/demo_security/manifests/tomcat.pp
+++ puppet/modules/demo_security/manifests/tomcat.pp
@@ -39,7 +39,7 @@ class demo_security::tomcat {
 
   $catalina_base = hiera('tomcat::catalina_home')
   $version       = hiera('tomcat::version')
-  $source_url    = "https://archive.apache.org/dist/tomcat/tomcat-7/v7.0.55/bin/apache-tomcat-${version}.tar.gz"
+  $source_url    = "https://archive.apache.org/dist/tomcat/tomcat-7/v${version}/bin/apache-tomcat-${version}.tar.gz"
 
   tomcat::instance { 'tomcat-demo':
     catalina_base => "$catalina_base",
