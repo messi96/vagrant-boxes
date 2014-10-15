e4330b0c249aa1c8050ef706d1e69042896445ec
Prateep Bandharangshi <prateep@waratek.com>
2014-10-15 10:52:01 +0100

    Remove demo_user check

--- puppet/modules/demo_security/manifests/init.pp
+++ puppet/modules/demo_security/manifests/init.pp
@@ -42,8 +42,5 @@ class demo_security (
   $kali_ip    = $demo_security::params::kali_ip
 ) inherits demo_security::params {
 
-  if ($demo_user == undef) {
-  	fail("Unable to setup demo")
-  }
 
 }
