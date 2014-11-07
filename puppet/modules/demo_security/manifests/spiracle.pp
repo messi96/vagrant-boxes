9799daf4b05856dd7e696d04895411b9b49f7873
Prateep Bandharangshi <prateep@waratek.com>
2014-11-07 14:35:38 +0000

    Initial commit

---
+++ puppet/modules/demo_security/manifests/spiracle.pp
@@ -0,0 +1,44 @@
+class demo_security::spiracle inherits demo_security {
+
+  include '::oracle_xe'
+
+  tomcat::war { 'spiracle.war':,
+    catalina_base => "$catalina_base",
+    war_source    => '/synced_folder/spiracle.war'
+  } ->
+
+  file { "${catalina_base}/webapps/spiracle":
+    ensure      => "directory",
+    owner       => "${demo_user}",
+    group       => "${demo_group}",
+    mode        => '0755'
+  } ->
+
+  exec { 'extract-spiracle-warfile':
+    command     => "/usr/bin/jar -xf ${catalina_base}/webapps/spiracle.war",
+    cwd         => "${catalina_base}/webapps/spiracle",
+    creates     => "${catalina_base}/webapps/spiracle/index.jsp",
+    user        => "${demo_user}",
+    group       => "${demo_group}",
+  } ~> 
+
+  exec { 'setup-db':
+    command     => "/u01/app/oracle/product/11.2.0/xe/bin/sqlplus SYS/testpass@//127.0.0.1:1521/XE AS SYSDBA < ${catalina_base}/webapps/spiracle/conf/setupdb.sql",
+    environment => [ 'ORACLE_HOME=/u01/app/oracle/product/11.2.0/xe',
+                     'ORACLE_SID=XE',
+                     'NLS_LANG=ENGLISH_IRELAND.AL32UTF8',
+                     'PATH=/u01/app/oracle/product/11.2.0/xe/bin:/usr/local/sbin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin' ],
+    refreshonly => true,
+    require     => Service['oracle-xe']
+  }
+
+  file { "${catalina_base}/webapps/spiracle/WEB-INF/lib/ojdbc6.jar":
+    ensure      => 'file',
+    owner       => "${demo_user}",
+    group       => "${demo_group}",
+    mode        => '0644',
+    source      => 'file:///u01/app/oracle/product/11.2.0/xe/jdbc/lib/ojdbc6.jar',
+    require     => Exec['extract-spiracle-warfile']
+  }
+
+}
