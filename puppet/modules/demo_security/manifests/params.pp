817091faa136e4e07349d4eb3506e7fd245e4c54
Prateep Bandharangshi <prateep@waratek.com>
2014-11-11 10:51:14 +0000

    Add default parameter of undef

--- puppet/modules/demo_security/manifests/params.pp
+++ puppet/modules/demo_security/manifests/params.pp
@@ -45,6 +45,6 @@ class demo_security::params {
   	$demo_group = $::vagrantuser
   }
 
-  $catalina_base  = hiera('tomcat::catalina_home')
-  $tomcat_version = hiera('tomcat::version')
+  $catalina_base  = hiera('tomcat::catalina_home', undef)
+  $tomcat_version = hiera('tomcat::version', undef)
 }
