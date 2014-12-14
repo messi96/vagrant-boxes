8fd185d5b505ac64c6132f66d9f23a30a5189046
Prateep Bandharangshi <prateep@waratek.com>
2014-12-14 14:31:41 +0000

    Renamed rules file, log and API call

--- puppet/modules/demo_security/manifests/centos.pp
+++ puppet/modules/demo_security/manifests/centos.pp
@@ -60,12 +60,12 @@ class demo_security::centos inherits demo_security {
     source => "puppet:///modules/demo_security/restart.sh",
   }
 
-  file { "/home/${demo_user}/demo/rules.jaf":
+  file { "/home/${demo_user}/demo/jvc.rules":
     ensure => "file",
     owner  => "${demo_user}",
     group  => "${demo_group}",
     mode   => 0644,
-    source => "puppet:///modules/demo_security/rules.jaf",
+    source => "puppet:///modules/demo_security/jvc.rules",
   }
 
   if ($::vagrant == "true") {
