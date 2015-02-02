f5826feee2cadf32178481ab31f0c6ca40ae4760
Prateep Bandharangshi <prateep@anicca.net>
2015-02-02 11:01:42 +0000

    Split long line

--- puppet/modules/demo_security/files/restart.sh
+++ puppet/modules/demo_security/files/restart.sh
@@ -2,7 +2,11 @@
 
 export JAVA_HOME="/usr/lib/jvm/java-waratek/jre"
 export JVM_OPTS="-Xmx512M -Xdaemon -Dcom.waratek.ssh.server=on"
-export JMX_OPTS="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=1234 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dcom.waratek.jmxhttp.jolokia"
+export JMX_OPTS="-Dcom.sun.management.jmxremote \
+-Dcom.sun.management.jmxremote.port=1234 \
+-Dcom.sun.management.jmxremote.authenticate=false \
+-Dcom.sun.management.jmxremote.ssl=false \
+-Dcom.waratek.jmxhttp.jolokia"
 export PATH=$JAVA_HOME/bin:$PATH
 
 echo "[*] Killing existing JVMs"
