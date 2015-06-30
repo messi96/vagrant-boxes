231944af772566c20e05dd70073d33c2e3470367
Prateep Bandharangshi <prateep@anicca.net>
2015-06-30 17:57:40 +0100

    Update log stash config

--- puppet/modules/demo_security/manifests/logstash_config.pp
+++ puppet/modules/demo_security/manifests/logstash_config.pp
@@ -1,11 +1,7 @@
 class demo_security::logstash_config inherits demo_security {
 
-  file { '/etc/logstash/conf.d':
-    ensure  => 'directory',
-    source  => 'puppet:///modules/demo_security/logstash/conf.d',
-    recurse => 'true',
-    owner   => 'root',
-    group   => 'root'
+  logstash::configfile { 'logstash.conf':
+    source => 'puppet:///modules/demo_security/logstash/conf.d/logstash.conf',
   }
 
 }
