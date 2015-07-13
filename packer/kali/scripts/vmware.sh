1cf6dbbb022bb94f15d15ed81f80deb9c41ccaeb
Prateep Bandharangshi <prateep@anicca.net>
2015-07-13 16:05:08 +0100

    Install open vm tools via hiera

--- packer/kali/scripts/vmware.sh
+++ packer/kali/scripts/vmware.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-# VMware tools
-#
-# http://docs.kali.org/general-use/install-vmware-tools-kali-guest
-
-set -o nounset
-set -o errexit
-
-echo "[*] Installing VMware tools"
-
-apt-get -y install open-vm-toolbox
 
 mkdir -p /mnt/hgfs
