b0330861afdc8422988edcbaaaec0d21376c61ff
Prateep Bandharangshi <prateep@anicca.net>
2015-07-29 18:28:02 +0100

    Update bootstrap process for AWS

--- scripts/cloud-init.sh
+++ scripts/cloud-init.sh
@@ -18,7 +18,3 @@ echo "" >> /etc/ssh/sshd_config
 echo "" >> /etc/ssh/sshd_config
 echo "UseDNS no" >> /etc/ssh/sshd_config
 
-# Install puppet
-rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm
-yum -y install puppet
-
