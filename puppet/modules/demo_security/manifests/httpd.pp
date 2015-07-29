e27b974a424b8ce4be90a84642b79ad25cca9eab
Prateep Bandharangshi <prateep@anicca.net>
2015-07-29 11:14:02 +0100

    set owner and group

--- puppet/modules/demo_security/manifests/httpd.pp
+++ puppet/modules/demo_security/manifests/httpd.pp
@@ -2,6 +2,8 @@ class demo_security::httpd inherits demo_security {
 
   file { '/etc/httpd/conf.d/demo_vhost.conf':
     ensure      => 'file',
+    owner       => 'root',
+    group       => 'root',
     mode        => '0644',
     source      => 'puppet:///modules/demo_security/demo_vhost.conf',
   } ~>
