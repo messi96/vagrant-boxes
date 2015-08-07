10f441e623cbde5b1204224a0b27eb2b8ae821d9
Prateep Bandharangshi <prateep@anicca.net>
2015-08-07 17:02:49 +0100

    Change hostname

--- scripts/kali-bootstrap.sh
+++ scripts/kali-bootstrap.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-set -e
-
 if [ "$EUID" -ne "0" ]; then
   echo "This script must be run as root." >&2
   exit 1
+fi
+
+grep -q compute.internal /etc/resolv.conf
+if [ $? -eq 0 ];then
+  echo "Changing EC2 hostname"
+  sed -i.orig -r 's/^search.*\.compute.internal/search aws.example.com/' /etc/resolv.conf
+  echo kali > /etc/hostname
+  echo "127.0.0.1 kali" >> /etc/hosts
+  /etc/init.d/hostname.sh start
 fi
 
 apt-key adv --keyserver hkp://keys.gnupg.net --recv-keys 7D8D0BF6 > /dev/null 2>&1
