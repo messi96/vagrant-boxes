cd8f3a4e0dee897c91422951a13f0ff895a4ec68
Prateep Bandharangshi <prateep@anicca.net>
2015-09-17 15:11:28 +0100

    Hack to prevent Oracle XE binding to IPv6 interface

---
+++ puppet/modules/demo_security/manifests/oracle_xe.pp
@@ -0,0 +1,49 @@
+# == Class: demo_security
+#
+# Full description of class demo_security here.
+#
+# === Parameters
+#
+# Document parameters here.
+#
+# [*sample_parameter*]
+#   Explanation of what this parameter affects and what it defaults to.
+#   e.g. "Specify one or more upstream ntp servers as an array."
+#
+# === Variables
+#
+# Here you should define a list of variables that this module would require.
+#
+# [*sample_variable*]
+#   Explanation of how this variable affects the funtion of this class and if
+#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
+#   External Node Classifier as a comma separated list of hostnames." (Note,
+#   global variables should be avoided in favor of class parameters as
+#   of Puppet 2.6.)
+#
+# === Examples
+#
+#  class { demo_security:
+#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
+#  }
+#
+# === Authors
+#
+# Author Name <author@domain.com>
+#
+# === Copyright
+#
+# Copyright 2014 Your name here, unless otherwise noted.
+#
+class demo_security::oracle_xe inherits demo_security {
+
+    # Hack to prevent Oracle XE binding to IPv6 interface
+
+    host { 'localhost-ipv6':
+        name   => 'localhost',
+        ensure => 'absent',
+        ip     => '::1',
+        notify => Service[oracle-xe]
+    }
+
+}
