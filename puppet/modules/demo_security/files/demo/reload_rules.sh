a85164cf92d26cdc1bf573e8cff20938b7bf1737
Prateep Bandharangshi <prateep@anicca.net>
2015-11-04 09:59:05 +0000

    Update reload script

--- puppet/modules/demo_security/files/demo/reload_rules.sh
+++ puppet/modules/demo_security/files/demo/reload_rules.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
 echo "[*] Reloading Security Rules"
-curl -s http://localhost:7777/jolokia/exec/com.waratek:type=VirtualContainer,jvcName=jvc-1/loadSecurityRules| python -mjson.tool
+RESPONSE=`curl -s http://localhost:7777/jolokia/exec/com.waratek:type=VirtualContainer,jvcName=jvc-1/loadSecurityRules| python -mjson.tool`
+STATUS=`echo "$RESPONSE" | grep status`
 
+echo "$RESPONSE"
+
+if [[ "$STATUS" =~ "200" ]]; then
+    exit 0
+else
+    exit 1
+fi
