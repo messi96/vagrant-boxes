31593e639a0e933a70bc5df0b8a6f0b3ad57cc2d
Prateep Bandharangshi <prateep@waratek.com>
2016-02-26 12:04:51 +0000

    Change rule variable

--- puppet/modules/waratek_rules/manifests/init.pp
+++ puppet/modules/waratek_rules/manifests/init.pp
@@ -1,17 +1,29 @@
 class waratek_rules(
-  $app_rules      = undef,
-  $default_rules  = undef,
-  $reload_script  = undef,
-  $rulefile_group = 'root',
-  $rulefile_mode  = '0600',
-  $rulefile_name  = undef,
-  $rulefile_owner = 'root',
-  $sqli_action    = 'deny',
-  $sqli_loglevel  = 'warn',
-  $xss_action     = 'deny',
-  $xss_loglevel   = 'warn'
+  $app_rules       = undef,
+  $default_rules   = undef,
+  $reload_script   = undef,
+  $rulefile_group  = 'root',
+  $rulefile_mode   = '0600',
+  $rulefile_name   = undef,
+  $rulefile_owner  = 'root',
+  $sqli_protection = true,
+  $sqli_loglevel   = 'warn',
+  $xss_protection  = true,
+  $xss_loglevel    = 'warn'
 
 ) {
+
+  if $sqli_protection {
+    $sqli_action = 'deny'
+  } else {
+    $sqli_action = 'allow'
+  }
+
+  if $xss_protection {
+    $xss_action = 'deny'
+  } else {
+    $xss_action = 'allow'
+  }
 
   if $rulefile_name {
     file { "$rulefile_name":
